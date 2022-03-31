e7b5baa7e05bb31ee4669de05361aadd51e21965
SC3 <ceosmarck@gmail.com>
2022-03-31 10:00:38 -0600

    segundo_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 
 function App() {
+
+  const [cuenta, setCuenta] = useState(0);
+
+
+
+  const handleClick = () => {
+    setCuenta(cuenta + 1)
+    //console.log("Diste click");
+  }
+
   return (
     <div className="App">
- 
+ <h3 className="text-center">Contador</h3>
+ <hr />
+ <h2 className="text-center">{cuenta}</h2>
+ <hr />
+ <div style={{display: "flex",
+  justifyContent: "flex-end",
+  marginRight: "5px",
+  }}>
+ <button type="button" className="btn btn-primary"
+ onClick={handleClick}>+1</button>
+ </div>
     </div>
   );
 }
